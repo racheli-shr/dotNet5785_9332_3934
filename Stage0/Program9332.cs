49c72ec6f5206030732f892f5ec33d56803b8fc6
Yael-Bloch <Yaelbloch2023@gmail.com>
2024-09-23 17:13:54 +0300

    initiall code+welcome function

---
+++ Stage0/Program9332.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome3934();
+            Welcome9332();
+            Console.ReadKey();
+        }
+        static partial void Welcome9332();
+        private static void Welcome3934()
+        {
+            Console.WriteLine("enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ",wehvdgsghdsg");
+        }
+    }
+}
