ee22bfff93ce3b6437d77971daba98a4133a982f
Yael-Bloch <yaelbloch2023@gmail.com>
2024-11-21 19:39:26 +0200

    call another changes

--- DalFacade/DO/Call.cs
+++ DalFacade/DO/Call.cs
@@ -3,22 +3,33 @@
 
 using static DO.Volunteer;
 using System.Numerics;
+using Microsoft.VisualBasic;
 
 namespace DO;
-
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Id">id of the volunteer</param>
+/// <param name="CallType"> </param>
+/// <param name="Description"></param>
+/// <param name="FullAdress"></param>
+/// <param name="Latitude"></param>
+/// <param name="Longitude"></param>
+/// <param name="OpeningCallTime"></param>
+/// <param name="MaxTimeToEnd"></param>
 public record Call
+(
+    int Id,
+    CallType CallType,  // הגדרת enum עם סוג "CallType"
+    string Description,
+    string FullAdress,
+    double Latitude,
+    double Longitude,
+    DateTime OpeningCallTime,
+    DateTime MaxTimeToEnd
+)
 {
-    int Id;
-    enum CallType;
-    string Description;
-    string FullAdress;
-    double Latitude;
-    double Longitude;
-    DateTime OpeningCallTime;
-    DateTime MaxTimeToEnd;
-
-
-    public Call(){ }
-
+    // בנאי ברירת מחדל שמאתחל את הערכים
+    public Call() : this(0,, "No description", "Unknown", 0.0, 0.0, DateTime.Now, DateTime.Now.AddHours(1)) { }
 }
 
