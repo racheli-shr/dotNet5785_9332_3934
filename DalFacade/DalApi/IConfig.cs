806fe0f48f6060848404f9b2d4da5e3e53336e4c
racheli-shr <gititshr@gmail.com>
2024-11-24 17:43:47 +0200

    finish

---
+++ DalFacade/DalApi/IConfig.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DalApi;
+
+public interface IConfig
+{
+    DateTime Clock { get; set; }
+    int NextCallId { get; set; }
+   int NextAssignmentId { get; set; }
+    TimeSpan RiskRange { get; set; }
+    void Reset();
+}
---
+++ DalList/ConfigImplementation.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Dal;
+
+public class ConfigImplementation: IConfig
+{
+    public DateTime Clock
+    {
+        get => Config.Clock;
+        set => Config.Clock = value;
+    }
+    public TimeSpan RiskRange
+    {
+        get => Config.RiskRange;
+        set => Config.RiskRange = value;
+    }
+    public int NextAssignmentId
+    {
+        get => Config.NextAssignmentId;
+        set => Config.NextAssignmentId = value;
+    }
+    public int NextCallId
+    {
+        get => Config.NextCallId;
+        set => Config.NextCallId = value;
+    }
+    public void Reset()
+    {
+        Config.Reset();
+    }
+}
