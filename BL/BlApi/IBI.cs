064a4df66d6b6654fa10a0676c5d239fdd8fa315
racheli-shr <gititshr@gmail.com>
2025-01-19 11:04:27 +0200

    Added IBl interface with readonly properties for ICall, IVolunteer, and IAdmin (8 -b)

---
+++ BL/BlApi/IBI.cs
@@ -0,0 +1,16 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi;
+
+public interface IBl
+{
+    ICall Call { get; }
+    IVolunteer Volunteer { get; }
+    IAdmin Admin { get; }
+}
+
