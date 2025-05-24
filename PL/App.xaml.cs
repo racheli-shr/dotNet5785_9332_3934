3ea407d3bac396f1621cd7f735a4c94f8a77a32d
racheli-shr <gititshr@gmail.com>
2025-05-24 23:57:59 +0300

    I added comments to the whole project  to explaining the code. and created tag

--- PL/App.xaml.cs
+++ PL/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Windows;
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction  logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
